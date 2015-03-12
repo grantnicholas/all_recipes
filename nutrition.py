4bded490b6403afc21d0e4fb751d9a507e61aea0
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2015-03-11 20:25:36 -0500

    Bug fixes

--- nutrition.py
+++ nutrition.py
@@ -1,23 +1,21 @@
-
-low_fat_ingredient_sub = {
-    'ground beef':'lean ground turkey',
+import re
+low_fat_ingredient_sub = { 
     'bacon':'canadian bacon',
     'sausage':'lean ham',
     'chicken':'skinless chicken',
     'turkey':'skinless turkey',
     'duck':'skinless turkey',
     'goose':'skinless turkey',
-    'beef chuck':'beef loin',
-    'beef rib':'beef loin',
-    'beef brisket':'beef loin',
-    'pork spareribs':'pork tenderloin',
-    'chorizo sausage':'turkey sausage',
+    'rib':'beef loin',
+    'brisket':'beef loin',
+    'spareribs':'pork tenderloin',
+    'sausage':'turkey sausage',
     'milk':'skim milk',
     'cream':'evaporated skim milk',
     'iceberg lettuce':'arugula',
     'butter':'cooking spray',
     'margarine':'olive oil',
-    'vegetable oil':'olive oil',
+    'oil':'olive oil',
     'shortening':'fat-free margarine',
     'soy sauce':'low-sodium soy sauce',
     'alfredo':'marinara',
@@ -25,38 +23,31 @@ low_fat_ingredient_sub = {
     'sour cream':'Greek yogurt',
     'bread':'pita',
     'flour tortilla':'corn tortilla',
-    'white bread':'whole wheat bread',
+    'bread':'whole wheat bread',
     'mayonnaise':'Greek yogurt',
     'eggs':'egg whites',
     'cream cheese':'fat-free ricotta cheese',
-    'white rice':'brown rice'
+    'rice':'brown rice'
 }
 high_fat_ingredient_sub ={
-    'lean ground turkey':'ground beef',
+    'turkey':'ground beef',
     'canadian bacon':'bacon',
-    'lean ham':'sausage',
-    'skinless chicken':'chicken',
-    'skinless turkey':'turkey',
-    'turkey sausage':'chorizo sausage',
-    'skim milk':'whole milk',
-    'evaporated skim milk':'cream',
+    'ham':'sausage',
+    'sausage':'chorizo sausage',
+    'milk':'whole milk',
     'arugula':'iceberg lettuce',
-    'cooking spray':'butter',
-    'olive oil':'margarine',
-    'olive oil':'vegetable oil',
-    'fat-free margarine':'shortening',
-    'low-sodium soy sauce':'soy sauce',
-    'whole wheat pasta':'pasta',
-    'corn tortilla':'flour tortilla',
-    'whole wheat bread':'white bread',
-    'egg whites':'eggs',
-    'brown rice':'white rice'
+    'oil':'margarine',
+    'margarine':'shortening',
+    'tortilla':'flour tortilla',
+    'bread':'white bread',
+    'whites':'eggs',
+    'rice':'white rice'
 }
 low_fat_method_sub = {
     'fry':'bake',
     'boil':'steam',
     'deep-fry':'bake',
-    'pan fry':'bake'
+    'fry':'bake'
 }
 high_fat_method_sub = {
     'bake':'fry',
@@ -108,25 +99,133 @@ salt_list = {
 "sugo alla puttanesca",
 }
 
+testrecipe = {
+        "ratings": {
+            "1": "4 cooks couldn't eat it",
+            "3": "25 cooks thought it was OK",
+            "2": "11 cooks didn't like it",
+            "5": "311 cooks loved it!",
+            "4": "82 cooks liked it!"
+        },
+        "name": "Smoky Grilled Pork Chops",
+        "nutrition": [
+            {
+                "units": "254 kcal",
+                "percentages": "13%",
+                "categories": "Calories"
+            },
+            {
+                "units": "5.4 g",
+                "percentages": "2%",
+                "categories": "Carbohydrates"
+            },
+            {
+                "units": "66 mg",
+                "percentages": "22%",
+                "categories": "Cholesterol"
+            },
+            {
+                "units": "14.3 g",
+                "percentages": "22%",
+                "categories": "Fat"
+            },
+            {
+                "units": "1.2 g",
+                "percentages": "5%",
+                "categories": "Fiber"
+            },
+            {
+                "units": "25 g",
+                "percentages": "50%",
+                "categories": "Protein"
+            },
+            {
+                "units": "773 mg",
+                "percentages": "31%",
+                "categories": "Sodium"
+            }
+        ],
+        "ingredients": [
+            {
+                "amount": "1 tablespoon",
+                "name": "seasoned salt (such as LAWRY'S\u00ae)"
+            },
+            {
+                "amount": "1 pound",
+                "name": "bacon"
+            },
+            {
+                "amount": "9 2/3 tablespoon",
+                "name": "garlic powder"
+            },
+            {
+                "amount": "1 tablespoon",
+                "name": "onion powder"
+            },
+            {
+                "amount": "1 tablespoon",
+                "name": "ground paprika"
+            },
+            {
+                "amount": "2 teaspoons",
+                "name": "Worcestershire sauce"
+            },
+            {
+                "amount": "1 teaspoon",
+                "name": "liquid smoke flavoring"
+            },
+            {
+                "amount": "4",
+                "name": "bone-in pork chops (1/2 to 3/4 inch thick)"
+            }
+        ],
+        "primary_method": "bake",
+        "directions": [
+            "Preheat an outdoor grill for medium heat, and lightly oil the grate.",
+            "In a bowl, mix together the seasoned salt, black pepper, garlic powder, onion powder, paprika, Worcestershire sauce, and smoke flavoring until thoroughly combined. Rinse pork chops, and sprinkle the wet chops on both sides with the spice mixture. With your hands, massage the spice rub into the meat; allow to stand for 10 minutes.",
+            "Grill the chops over indirect heat until no longer pink inside, about 12 minutes per side. An instant-read thermometer should read at least 145 degrees F (63 degrees C). Allow chops to stand for 10 more minutes before serving."
+        ],
+        "footnotes": []
+    }
+
+#takes the main dictionary section of the recipe (the value corresponding to the URL key) and lowers the fat in the ingredients
 def increase_fat(recipe):
     for ingredient in recipe["ingredients"]:
         if ingredient["name"] in high_fat_ingredient_sub.keys():
             curr_ingredient = ingredient["name"]
             alt_ingredient = high_fat_ingredient_sub[curr_ingredient]
             ingredient["name"] = alt_ingredient
-    # TODO Replace methods using high_fat_method_sub
+    curr_method = recipe["primary_method"]
+    if curr_method in high_fat_method_sub.keys():
+        recipe["primary_method"] = high_fat_method_sub[curr_method]
     return recipe
 
+#takes the main dictionary section of the recipe (the value corresponding to the URL key) and lowers the fat in the ingredients
 def decrease_fat(recipe):
     for ingredient in recipe["ingredients"]:
         if ingredient["name"] in low_fat_ingredient_sub.keys():
             curr_ingredient = ingredient["name"]
             alt_ingredient = low_fat_ingredient_sub[curr_ingredient]
             ingredient["name"] = alt_ingredient
-    # TODO Replace methods using low_fat_method_sub
+    curr_method = recipe["primary_method"]
+    if curr_method in low_fat_method_sub.keys():
+        recipe["primary_method"] = low_fat_method_sub[curr_method]
     return recipe
 
-def decrease_salt(recipe):
-    for ingredient in recipe["ingredients"]:
-        if ingredient["name"] in salt_list.keys():
-            # TODO decrease ingredient aount somehow
+# def decrease_salt(recipe):
+#     for ingredient in recipe["ingredients"]:
+#         for line in ingredient["amount"]:
+#             numbers = re.findall('\d+\.*\/*\d*', line)
+#             if numbers:
+#                 print numbers
+
+
+
+
+
+def main():
+    test = decrease_salt(testrecipe)
+    print test
+
+if __name__ == '__main__':
+    main()
--- vegetarian.py
+++ vegetarian.py
@@ -1,3 +1,4 @@
+import re
 meat_list =    {'chicken': 'seitan',
 				'veal': 'tofu',
 				'beef': 'beans',
@@ -146,43 +147,155 @@ fish_list = {
 				'trout': 'tofu',
 				'shrimp': 'tofu',
 				'tuna': 'tofu'}
+testrecipe = {
+     "ratings": {
+            "1": "not found",
+            "3": "1 cook thought it was OK",
+            "2": "not found",
+            "5": "47 cooks loved it!",
+            "4": "7 cooks liked it!"
+        },
+        "name": "Ham and Hash Brown Quiche",
+        "nutrition": [
+            {
+                "units": "286 kcal",
+                "percentages": "14%",
+                "categories": "Calories"
+            },
+            {
+                "units": "16.2 g",
+                "percentages": "5%",
+                "categories": "Carbohydrates"
+            },
+            {
+                "units": "153 mg",
+                "percentages": "51%",
+                "categories": "Cholesterol"
+            },
+            {
+                "units": "23.2 g",
+                "percentages": "36%",
+                "categories": "Fat"
+            },
+            {
+                "units": "1.4 g",
+                "percentages": "6%",
+                "categories": "Fiber"
+            },
+            {
+                "units": "13.8 g",
+                "percentages": "28%",
+                "categories": "Protein"
+            },
+            {
+                "units": "663 mg",
+                "percentages": "27%",
+                "categories": "Sodium"
+            }
+        ],
+        "ingredients": [
+            {
+                "amount": "3 cups",
+                "name": "salmon"
+            },
+            {
+                "amount": "3 tablespoons",
+                "name": "butter, melted"
+            },
+            {
+                "amount": "1 cup",
+                "name": "cooked ham, chopped"
+            },
+            {
+                "amount": "1 cup",
+                "name": "shredded Cheddar cheese"
+            },
+            {
+                "amount": "1/4 cup",
+                "name": "chopped onion"
+            },
+            {
+                "amount": "1/4 cup",
+                "name": "chopped green bell pepper"
+            },
+            {
+                "amount": "3",
+                "name": "large eggs"
+            },
+            {
+                "amount": "1/2 cup",
+                "name": "milk"
+            },
+            {
+                "amount": "1/2 teaspoon",
+                "name": "salt"
+            },
+            {
+                "amount": "1/4 teaspoon",
+                "name": "ground black pepper"
+            }
+        ],
+        "directions": [
+            "Preheat oven to 425 degrees F (220 degrees C).",
+            "Press hash brown potatoes between paper towels to remove excess moisture. Transfer potatoes to an 8-inch pie dish and press into the bottom and up the sides of the dish to form a crust. Drizzle with melted butter.",
+            "Bake in preheated oven until golden, about 25 minutes. Remove from oven and reduce heat to 350 degrees F (175 degrees C).",
+            "Combine ham, Cheddar cheese, onion, and bell pepper in a bowl; spread mixture over potato crust. Beat eggs, milk, salt, and black pepper in the same bowl; pour over ham mixture.",
+            "Bake in oven until eggs are set, 25 to 30 minutes."
+        ],
+        "footnotes": []
+    }
 
 def is_vegetarian(recipe):
 	for ingredient in recipe["ingredients"]:
-		if ingredient["name"] in meat_list.keys():
-			return True
+		for split_ing in ingredient["name"].split():
+			split_ing = re.sub('[;,.]', '', split_ing)
+			if split_ing in meat_list.keys():
+				return True
 	return False
 
 def make_vegetarian(recipe):
 	for ingredient in recipe["ingredients"]:
-		if ingredient["name"] in meat_list.keys():
-			curr_meat = ingredient["name"]
-			alt_meat = meat_list[curr_meat]
-			ingredient["name"] = alt_meat
+		for split_ing in ingredient["name"].split():
+			split_ing = re.sub('[;,.]', '', split_ing)
+			if split_ing in meat_list.keys():
+				curr_meat = ingredient["name"]
+				alt_meat = meat_list[split_ing]
+				ingredient["name"] = alt_meat
 	return recipe
 
 def make_meat(recipe):
 	for ingredient in recipe["ingredients"]:
-		for meat, sub in meat_list.iteritems():
-			if sub == ingredient["name"]:
-				ingredient["name"] = sub
-				break
+		for split_ing in ingredient["name"].split():
+			split_ing = re.sub('[;,.]', '', split_ing)
+			for meat, sub in meat_list.iteritems():
+				if sub == split_ing:
+					ingredient["name"] = meat
+					break
 	return recipe
 
 def is_pescatarian(recipe):
 	for ingredient in recipe["ingredients"]:
-		if ingredient["name"] in meat_list.keys():
-			return True
+		for split_ing in ingredient["name"].split():
+			split_ing = re.sub('[;,.]', '', split_ing)
+			if split_ing in meat_list.keys() and split_ing not in fish_list.keys():
+				return True
 	return False
 
 def make_pescatarian(recipe):
 	for ingredient in recipe["ingredients"]:
-		if ingredient["name"] in meat_list.keys() and ingredient["name"] not in fish_list.keys():
-			curr_meat = ingredient["name"]
-			alt_meat = meat_list[curr_meat]
-			ingredient["name"] = alt_meat
+		for split_ing in ingredient["name"].split():
+			split_ing = re.sub('[;,.]', '', split_ing)
+			if split_ing in meat_list.keys() and split_ing not in fish_list.keys():
+				curr_meat = ingredient["name"]
+				alt_meat = meat_list[split_ing]
+				ingredient["name"] = alt_meat
 	return recipe
 
 
+def main():
+    test = make_meat(make_vegetarian(testrecipe))
+    print test
 
+if __name__ == '__main__':
+    main()
 

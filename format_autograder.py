1957103e745eadbf3fdca66d3c43b70e0f593d0f
grant nicholas <grantnicholas2015@u.northwestern.edu>
2015-03-16 15:32:44 -0500

    improved accuracy for autograder

--- format_autograder.py
+++ format_autograder.py
@@ -6,6 +6,46 @@ from crawlr import Crawler
 from pprint import pprint
 
 
+#Handles fractions 
+def str_to_num(astring):
+    if "/" in astring:
+        try:
+            num, denom = astring.split("/")
+            f_num = float(num)
+            f_denom = float(denom)
+            return f_num / f_denom
+        except:
+            return float(astring)
+    else:
+        return float(astring)
+
+
+def get_name_descriptor(name):
+    print 'name:', name
+    if "," in name:
+        try:
+            ingred, descriptor = name.split(",")
+            print ingred, descriptor
+            return ingred, descriptor
+        except:
+            return name, ""
+    else:
+        return name, ""
+
+
+def get_quantity_measurement(quantity):
+    if quantity != "not found":
+        arglist = quantity.split(" ")
+        if len(arglist) >= 2:
+            return str_to_num(arglist[0]), arglist[1]
+        elif len(arglist) >= 1:
+            return str_to_num(arglist[0]), "not found"
+        else:
+            return "not found", "not found"
+    else:
+        return "not found", "not found"
+
+
 def create_recipe_data_structures(ingred_dict, saved_file, final_structure_file):
     tools = ["absinthe spoon", "adjustable v-rack", "aluminum foil", "aluminum stock pan", "anodized aluminum nonstick pan", "apple corer", "apple cutter", "auto reignition", "bachelor griller", "bamboo stove", "bar spoon", "barbecue", "barbecue grill", "basket rack", "baster", "beehive oven", "berry spoon", "big green egg", "biscuit cutter", "biscuit press", "blow torch", "boil over preventer", "baking pan", "baking sheets", "baking stone", "beanpot", "bedrock mortar", "bottle opener", "bottle scraper", "bowl", "braiser pan", "brasero", "brazier pot", "bread knife", "bread pan", "bread machine", "broiler pan", "browning tray", "burjiko", "butane torch", "butcher block", "butcher paper", "butter churn", "butter curler", "caddy spoon", "cake and pie server", "cake pan", "can opener", "candy thermometer", "carbon steel pan", "casserole", "casserole pan", "cassole", "cast iron skillet", "cast iron dutch oven", "chakla", "chambers stove", "cheesecloth", "cheesemelter", "cheese knife", "cheese scoop", "chef's knife", "chef's pan", "cherry pitter", "chestnut pan", "chinoise", "chip pan", "chocolatera", "churchkey", "cocktail stick", "coffee filter", "coffeemaker", "colander", "convection microwave", "cookie cutter", "cookie press", "cookie sheet", "cooking pot", "cooking sheet", "cooling racks", "copper hand hammered brazing pot", "copper sauce pan", "corkscrew", "crab cracker", "crepe pan", "cutting board", "chorkor oven", "clean-burning stove", "clome oven", "coffee percolator", "coffeemaker",
              "comal (cookware)", "combi steamer", "communal oven", "community cooker", "convection microwave", "convection oven", "cook stove", "corn roaster", "crepe maker", "diffuser (heat)", "double boiler", "doufeu", "dough blender", "dough scraper", "dutch oven", "deep fryer", "eggbeater", "egg piercer", "egg poacher", "egg separator", "egg slicer", "egg timer", "electric kettle", "electric water boiler", "embossing mat", "earth oven", "ecozoom", "electric cooker", "electric stove", "energy regulator", "fillet knife", "fish scalar", "fish slice (kitchen utensil)", "flat iron grill pan", "flat rack", "flesh-hook", "flour sifter", "fondue pot", "food mill", "food processor", "frosting spatula", "frying pan", "frying skillet", "funnel", "field kitchen", "fire pot", "flattop grill", "food steamer", "foukou", "fufu machine", "gas stove", "garlic press", "glass baking pan", "gratine pan", "grapefruit knife", "grater", "gravy strainer", "greaseproof paper", "griddle", "grill pan", "square griddle pan", "hand held electric mixer", "herb chopper", "halogen oven", "haybox", "hibachi", "hoang cam stove", "hobo stove", "horno", "hot box (appliance)", "hot plate", "induction cooking", "juicer", "karahi", "kettle", "kitchen utensil", "kitchen knife", "kamado", "kettle", "kitchen oven", "kitchen stove", "kitchener range", "krampouz", "kujiejun", "kyoto box", "ladle", "lame", "lemon reamer", "lemon squeezer", "loaf pan", "lobster pick", "lo trau", "mandoline", "mated colander pot", "measuring cup", "measuring jug", "measuring spoon", "meat grinder", "meat tenderizer", "meat thermometer", "meatloaf pan", "melon baller", "mezzaluna", "mortar and pestle", "microplane", "microwave", "microwave oven", "milk watcher", "mouli grater", "mushroom cloth", "masonry oven", "mess kit", "microwave oven", "multi-fuel stove", "nonadjustable v-rack", "nonstick skillet", "nutcracker", "nutmeg grater", "nomiku", "omelette pan", "oven", "oven bag", "oven glove", "oxo (brand)", "p-38 can opener", "paella pan", "palayok", "parchment paper", "pasta spoon", "pastry bag", "pastry blender", "pastry brush", "pastry wheel", "peel (tool)", "peeler", "pepper mill", "pie bird", "pie pan", "pizza cutter", "plastic wrap", "potato masher", "potato ricer", "pot boiler", "pot holder", "pothook", "poultry shears", "pressure cooker", "pudding basin", "pudding cloth", "pyrex baking dish", "popcorn maker", "primus stove", "ramekin", "rice spoon", "ricer", "rice cooker", "roasting jack", "roasting pan", "roasting pan with high cover", "roasting rack", "roller docker", "rolling pin", "rondeau", "round cake pan", "red cross stove", "reflector oven", "remoska", "rice cooker", "rice polisher", "roasting jack", "rocket mass heater", "rocket stove", "russian oven", "salad spoon", "salt and pepper shakers", "saran (plastic)", "saucier", "saute pan", "saucepan", "sauteuse pan", "scales", "scissors", "scoop", "scotch hands", "scraper (kitchen)", "serving spoon", "serving tongs", "shellfish scraper", "sieve", "silpat", "skewer", "skillet", "skimmer", "slotted spoon", "soup ladle", "spatula", "spider", "splatter screen", "springform pan", "spurtle", "stainless steel pan", "steel saute pan", "stir-fry pan", "stockpot", "sugar nips", "sugar spoon", "sugar thermometer", "sugar tong", "sujeo", "sabbath mode", "salamander broiler", "samovar", "sandwich toaster", "self-cleaning oven", "shichirin", "sigri (stove)", "slow cooker", "solar cooker", "soy milk maker", "stove", "susceptor", "tablespoon", "tajine", "tamis", "tava", "tea infuser", "teflon coated frying pan", "timbale (food)", "tin foil", "tin opener", "toaster oven", "tomato knife", "tongs", "trivection oven", "trivet", "trussing needle", "two burner griddle pan", "tabun oven", "tandoor", "tangia", "tea stove", "thermal immersion circulator", "toaster", "tommy cooker", "trojan room coffee pot", "turkey fryer", "vegetable peeler", "vertical rack", "vacuum fryer", "wax paper", "whisk", "wok", "wonder pot", "wooden spoon", "waffle iron", "wet grinder", "wood-burning stove", "wood-fired oven", "zester", "stock pot", "zipper bag", "shallow dish"]
@@ -43,6 +83,19 @@ def create_recipe_data_structures(ingred_dict, saved_file, final_structure_file)
                     if method in direction:
                         if method not in secondary_method:
                             secondary_method.append(method)
+
+            for ingredient in recipe["ingredients"]:
+                quantity, measurement = get_quantity_measurement(ingredient["quantity"])
+                name, descriptor = get_name_descriptor(ingredient["name"])
+                ingredient["measurement"] = measurement
+                if ingredient["measurement"] == "not found":
+                    ingredient["measurement"] = "units"
+                ingredient["quantity"] = quantity
+                ingredient["descriptor"] = descriptor
+                ingredient["name"] = name
+                ingredient["preparation"] = ""
+                ingredient["prep-description"] = ""
+
             recipe_to_cuisine.append({"url": link,
                                       "name": recipe["name"],
                                       "ingredients": recipe["ingredients"],
